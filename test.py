9276f320dc246913f98489d36532bc0de13f4487
MelcherMate <melchermate28@gmail.com>
2025-03-25 02:00:24 +0100

    small changes

--- test.py
+++ test.py
@@ -10,9 +10,13 @@ def check_modbus_conncetion():
 		conncetion = client.connect()
 		
 		if conncetion:
+			print(f"------------------------------------------------------")
 			print(f"Successfully connected to PLC on ({PLC_IP}:{PLC_PORT})")
+			print(f"------------------------------------------------------")
 		else:
+			print(f"------------------------------------------------------")
 			print(f"Failed to connect to PLC on ({PLC_IP}:{PLC_PORT})")
+			print(f"------------------------------------------------------")
 			
 		#Close connection
 		client.close()
